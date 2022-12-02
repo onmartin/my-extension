97ba5045d0c79b3b37044ca69c11b726af3fa768
onmartin <118198239+onmartin@users.noreply.github.com>
2022-12-02 08:58:57 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+scene.setBackgroundColor(1)
+effects.blizzard.startScreenEffect()
+pause(3000)
+game.setDialogFrame(img`
+    .....cccccccccccccc.....
+    ...cbd111111111111dbc...
+    ..cd1111111111111111dc..
+    .cd111111111111111111dc.
+    .b11111111111111111111b.
+    cd11111111111111111111dc
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    c1111111111111111111111c
+    cd11111111111111111111dc
+    cb11111111111111111111bc
+    ccd111111111111111111dc.
+    .ccd1111111111111111dcc.
+    ..c111111111111111dbcc..
+    .b11dcccccccccccccccc...
+    cddcccccccccccccccc.....
+    ccccc...................
+    `)
+game.setDialogTextColor(15)
+game.showLongText("Welcome to The War to End all Wars", DialogLayout.Center)
+effects.blizzard.endScreenEffect()
+music.powerUp.play()
